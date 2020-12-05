1ea8a70358e80482f1c4f5bf1b5ccddfa7eb46a3
FfrftTK <t1610415@uec.ac.jp>
2020-12-06 01:50:52 +0900

    Add Utils

---
+++ lib/utils/network/api_client.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:tag_data_manager_client/utils/utils.dart';
+
+class ApiClient {
+  factory ApiClient() {
+    // ignore: join_return_with_assignment
+    _instance ??= ApiClient._();
+
+    return _instance;
+  }
+  ApiClient._();
+
+  static ApiClient _instance;
+
+  Future<Result<T>> sendRequest<T>({
+    @required Future<Response<Map<String, dynamic>>> request,
+    @required T Function(Map<String, dynamic>) jsonDecodeCallback,
+  }) async {
+    try {
+      return await request
+          .then((value) => Result.success(jsonDecodeCallback(value.data)));
+    } on Exception catch (e) {
+      return const Result.failure(NetworkExceptions.badRequest());
+    }
+  }
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,6 @@
 library utils;
 
+export 'network/api_client.dart';
 export 'network/app_dio_client.dart';
 export 'network/error/network_exceptions.dart';
 export 'network/result/result.dart';
